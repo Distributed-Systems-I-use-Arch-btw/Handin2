45b034c260dc4b223039ff1ffbf0fd1d324c3453
mikcla <micla@me.com>
2024-09-18 21:42:10 +0200

    Very basic handshake
    To and from slide 29 :+1:

--- main.go
+++ main.go
@@ -1,9 +1,80 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	go server()
+	go client()
+
+	for {
+
+	}
+}
+
+func server() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	conn.Read(buffer) // Read seq
+
+	println("Server")
+	println((buffer[0]))
+
+	buffer[0] += 1
+
+	conn.Write(buffer)      // Send seq + 1
+	conn.Write([]byte{200}) // Send ack
+
+	conn.Read(buffer) // Read seq + 1
+
+	println("Server")
+	println((buffer[0]))
+
+	conn.Read(buffer) // Read ack + 1
+
+	println((buffer[0]))
+}
+
+func client() {
+	dial, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dial.Write([]byte{100}) // Send seq
+
+	buffer := make([]byte, 1024)
+
+	dial.Read(buffer) // Read seq + 1
+
+	println("Client")
+	println((buffer[0]))
+
+	dial.Write(buffer) // Write seq + 1
+	dial.Read(buffer)  // Read ack
+
+	println((buffer[0]))
+
+	buffer[0] += 1
+
+	dial.Write(buffer) // Send ack + 1
 }
