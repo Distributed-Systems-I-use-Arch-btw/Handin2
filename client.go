0635a5a434c37ac91102e39c83863209b425c9ed
ITU-mikcl <mikcl@itu.dk>
2024-09-21 20:16:55 +0200

    Send and retrieve data

--- client.go
+++ client.go
@@ -12,32 +12,53 @@ func client() {
 		log.Fatal(err)
 	}
 
-	//Makes the byte to send
+	// Makes the byte to send
 	seqSend := make([]byte, 1)
 	seqSend[0] = 100
 
 	// Send seq
 	dial.Write(seqSend)
 
-	//Make recivers
+	// Make recivers
 	ackReceived := make([]byte, 1)
 	seqRecived := make([]byte, 1)
 
-	//Reads recives values
+	// Reads recives values
 	dial.Read(ackReceived)  // Reads seq + 1
 	dial.Read(seqRecived) // Reads ack
 
-	//Checks that the correct value is recived
+	// Checks that the correct value is recived
 	if ackReceived[0] == (seqSend[0] + 1) {
 		fmt.Println("Correct ack recived")
 	} else {
 		fmt.Println("Not correct")
 	}
 
-	//Adds 1 to the seq recived
+	// Adds 1 to the seq recived
 	seqRecived[0] += 1
 	
-	//Sends back the seq + 1 and ack recived to establish the connection
+	// Sends back the seq + 1 and ack recived to establish the connection
 	dial.Write(seqRecived)
 	dial.Write(ackReceived)
+	
+	// Make data reciver
+	dataRecvier := make([]byte, 1024)
+	
+	// Retrive data and the length of data
+	n, err := dial.Read(dataRecvier)
+	if err != nil {
+        log.Fatal(err)
+    }
+	
+	// Print data
+    fmt.Println("Data recived:")
+	fmt.Println(string(dataRecvier[:n]))
+	
+	// Add 1 to the length of the data recived
+	dataAck := make([]byte, 1024)
+	
+	dataAck[0] = byte(n) + 1
+	
+	// Send data ack
+	dial.Write(dataAck)
 }
--- handleConnection.go
+++ handleConnection.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func handleConnection(conn net.Conn) {
@@ -12,25 +13,42 @@ func handleConnection(conn net.Conn) {
 
 	conn.Read(ack) // Read seq
 
-	//Adds 1 to the seq recived
+	// Adds 1 to the seq recived
 	ack[0] += 1
 
-	//Send the ack and seq
+	// Send the ack and seq
 	conn.Write(ack) // Sends ack
 	conn.Write(seq) // Send seq
 
-	//Reads the ack + 1 and seq + 1
+	// Reads the ack + 1 and seq + 1
 	seqRecived := make([]byte, 1)
 	ackRecived := make([]byte, 1)
 	
 	conn.Read(ackRecived)
 	conn.Read(seqRecived)
 
-	//Test that they are correct
+	// Test that they are correct
 	if ackRecived[0] == (seq[0]+1) && seqRecived[0] == ack[0] {
 		fmt.Println("Correct seq and ack recived")
 	    fmt.Println("Connection established")
 	} else {
 		fmt.Println("Not correct")
 	}
+
+    data := time.Now().String()
+
+    // Send data to client
+    conn.Write([]byte(data))
+    
+    receivedData := make([]byte, 1024)
+    
+    // Read data ack
+    conn.Read(receivedData)
+    
+    
+    if int(receivedData[0]) == len(data) + 1 {
+        fmt.Println("Data was recived correctly")
+    } else {
+        fmt.Println("Data was not recived correctly")
+    }
 }
